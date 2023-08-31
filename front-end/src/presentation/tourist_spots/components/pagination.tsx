1b67b5d5fbc905a3221c8b4987d8de182f5f8acc
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-08-31 14:15:17 -0300

    feat: pres: added loading animation

--- front-end/src/presentation/tourist_spots/components/pagination.tsx
+++ front-end/src/presentation/tourist_spots/components/pagination.tsx
@@ -10,21 +10,21 @@ export const Pagination = (props: PaginationProps) => {
   const maxPages = props.arrayLength / props.pageSize;
   return (
     <div className="flex flex-col items-center">
-      <span className="text-sm text-gray-700 dark:text-gray-400">
-      Página <span className="font-semibold text-gray-900 dark:text-white">{props.currentPage}</span> de <span
-        className="font-semibold text-gray-900 dark:text-white">{Math.ceil(maxPages)}</span>
+      <span className="text-sm text-gray-700">
+      Página <span className="font-semibold text-gray-900">{props.currentPage}</span> de <span
+        className="font-semibold text-gray-900">{Math.ceil(maxPages)}</span>
       </span>
       <div className="inline-flex mt-2 xs:mt-0">
         <button
           disabled={props.currentPage === 1}
-          className="flex items-center justify-center px-3 h-8 text-sm disabled:opacity-50 font-medium text-white bg-dark-blue rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center px-3 h-8 text-sm disabled:opacity-50 font-medium text-white bg-dark-blue rounded-l"
           onClick={props.onPreviousPage}
         >
           Anterior
         </button>
         <button
-          disabled={props.currentPage === Math.ceil(maxPages)}
-          className="flex items-center justify-center px-3 h-8 text-sm disabled:opacity-50 font-medium text-white bg-dark-blue border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          disabled={props.currentPage >= Math.ceil(maxPages)}
+          className="flex items-center justify-center px-3 h-8 text-sm disabled:opacity-50 font-medium text-white bg-dark-blue border-0 border-l border-gray-700 rounded-r"
           onClick={props.onNextPage}
         >
           Próximo
--- front-end/src/presentation/tourist_spots/tourist_spots.tsx
+++ front-end/src/presentation/tourist_spots/tourist_spots.tsx
@@ -10,6 +10,7 @@ import {useContext, useEffect, useState} from "react";
 import {ModalConfirmDelete} from "./components/modal_confirm_delete.tsx";
 import {SiteButton} from "../shared/components/site_button.tsx";
 import {Snackbar} from "../shared/components/snackbar.tsx";
+import {Loading} from "../shared/components/loading.tsx";
 
 export const TouristSpots = () => {
   return (
@@ -37,7 +38,6 @@ const _TouristSpots = () => {
       touristSpotsContext.deleteTouristSpot(id);
     }
   }
-
   const handleCancelDelete = () => {
     setOpenModal({isOpen: false, id: undefined})
   }
@@ -55,57 +55,60 @@ const _TouristSpots = () => {
   }, [openModal]);
 
   return (
-    <div className={"flex flex-col justify-between relative grow px-40 py-8 2xl:px-80"}>
-      <div>
+    <>
+      <div className={"flex flex-col justify-between relative grow px-40 py-8 2xl:px-80"}>
         <div className={"flex w-full gap-6 justify-between"}>
           <SiteInput placeholder={"Pesquisar..."} type={"text"} onChange={(e) => console.log(e.target.value)}/>
           <SiteButton text={"Buscar"} onClick={() => console.log("Buscar")}/>
         </div>
+        <div className={"h-full"}>
+          <div className={"flex flex-col h-full pt-6 gap-10"}>
+            {state.loading ?
+              <Loading/>
+              :
+              filteredTouristSpots.map((touristSpot) => (
+                <CardTouristSpot
+                  onDelete={() => setOpenModal({isOpen: true, id: touristSpot.id})}
+                  id={touristSpot.id as number}
+                  name={touristSpot.name}
+                  description={touristSpot.description}
+                  location={touristSpot.location}
+                  city={touristSpot.city}
+                  state={touristSpot.state}
+                />
+              ))}
 
-
-        {state.loading ?
-          <p>Carregando...</p>
-          :
-          <div className={"flex flex-col pt-6 gap-10"}>
-          {state.loading ?
-            <div>Carregando...</div>
-            :
-            filteredTouristSpots.map((touristSpot) => (
-              <CardTouristSpot
-                onDelete={() => setOpenModal({isOpen: true, id: touristSpot.id})}
-                id={touristSpot.id as number}
-                name={touristSpot.name}
-                description={touristSpot.description}
-                location={touristSpot.location}
-                city={touristSpot.city}
-                state={touristSpot.state}
-              />
-            ))}
+            {!filteredTouristSpots.length ?
+              <div className={"flex items-center justify-center"}>
+                <h1 className={"text-dark-blue font-bold"}>Nenhum ponto turístico encontrado</h1>
+              </div>
+              : null
+            }
+          </div>
         </div>
+
+        <Pagination
+          arrayLength={state.touristSpots.length}
+          currentPage={currentPage}
+          pageSize={5}
+          onNextPage={() => setCurrentPage(currentPage + 1)}
+          onPreviousPage={() => setCurrentPage(currentPage - 1)}
+        />
+
+
+        {openModal &&
+            <ModalConfirmDelete
+                openModal={openModal.isOpen}
+                onDelete={handleConfirmDelete}
+                onCancel={handleCancelDelete}
+            />
         }
       </div>
-
-      <Pagination
-        arrayLength={state.touristSpots.length}
-        currentPage={currentPage}
-        pageSize={5}
-        onNextPage={() => setCurrentPage(currentPage + 1)}
-        onPreviousPage={() => setCurrentPage(currentPage - 1)}
-      />
-
-      {openModal &&
-          <ModalConfirmDelete
-              openModal={openModal.isOpen}
-              onDelete={handleConfirmDelete}
-              onCancel={handleCancelDelete}
-          />
-      }
-
       <Snackbar
         type={"success"}
         message={state.success}
         open={!!state.success}
       />
-    </div>
+    </>
   )
 }
