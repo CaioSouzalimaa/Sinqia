97dfede8c2d2910d699e5a7a583f5c299b15f092
Caio Souza Lima <caiosouzalima2002@gmail.com>
2023-08-31 20:57:42 -0300

    fix: pres: desactive navbar button when is respective page

--- front-end/src/presentation/app/header_nav_bar.tsx
+++ front-end/src/presentation/app/header_nav_bar.tsx
@@ -3,7 +3,8 @@ import Logo from "../../assets/logo.png";
 
 export const HeaderNavBar = () => {
   const navigate = useNavigate();
-
+  const isAddPage = location.pathname === "/pontos-turisticos/adicionar" || location.pathname.startsWith("/pontos-turisticos/adicionar/");
+  const isHome = location.pathname === "/";
   return (
     <nav className={"flex flex-row sm:gap-10 px-8 md:px-40 2xl:px-80 py-3 w-full bg-dark-blue items-center sticky top-0 z-50 justify-between"}>
       <div>
@@ -11,11 +12,15 @@ export const HeaderNavBar = () => {
       </div>
       <div>
         <ul className={"flex flex-row w-full gap-2 sm:gap-10 text-[8px] sm:text-sm text-white"}>
-          <li className={`hover:cursor-pointer hover:text-gray-500`} onClick={() => navigate("/")}>
-            PONTOS TURÍSTICOS
+          <li className={`${isHome ? "text-gray-500" : "text-white"} hover:cursor-pointer hover:text-gray-500`}>
+            <button disabled={isHome} onClick={() => navigate("/")}>
+              PONTOS TURÍSTICOS
+            </button>
           </li>
-          <li className={"hover:cursor-pointer hover:text-gray-500"} onClick={() => navigate("/pontos-turisticos/adicionar")}>
-            CADASTRAR PONTO TURÍSTICO
+          <li className={`${isAddPage ? "text-gray-500" : "text-white"} hover:cursor-pointer hover:text-gray-500`}>
+            <button disabled={isAddPage} onClick={() => navigate("/pontos-turisticos/adicionar")}>
+              CADASTRAR PONTO TURÍSTICO
+            </button>
           </li>
         </ul>
       </div>
